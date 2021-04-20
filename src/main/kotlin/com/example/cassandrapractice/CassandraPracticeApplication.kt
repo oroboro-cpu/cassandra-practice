2709183344f3013e6885559a0115504bc99c23cd
yaroslav <yaroslav8oroboro@gnail.com>
2021-04-20 16:24:24 +0300

    first commit

---
+++ src/main/kotlin/com/example/cassandrapractice/CassandraPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cassandrapractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CassandraPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CassandraPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cassandrapractice/CassandraPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cassandrapractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CassandraPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
